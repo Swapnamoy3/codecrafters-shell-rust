38c23907017c59a23e860b741cd69943f120ccb7
Swapnamoy Garai <garaiswap27@gmail.com>
2026-02-05 10:25:56 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,26 @@ fn print_error(command: String, message: String) {
 
 }
 
+
+enum COMMANDS{
+    EXIT, 
+    ECHO(String),
+    ERROR(String, String),
+}
+fn parse_command(command: String) -> COMMANDS{
+    if(command.starts_with("exit")){
+        return COMMANDS::EXIT;
+    }
+    else if (command.starts_with("echo")) {
+        let rest = command[5..].to_string(); 
+        return COMMANDS::ECHO(rest);
+    }
+
+
+    return COMMANDS::ERROR(command, "command not found".to_string());
+    
+}
+
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
 
@@ -26,11 +46,12 @@ fn main() {
 
         let command = input_command();
 
-        if command == "exit" {
-            break;
-        }
-        
-        print_error(command, "command not found".to_string());
+        match parse_command(command.clone()){
+            COMMANDS::EXIT => break,
+            COMMANDS::ECHO(rest) => println!("{}", rest),
+            COMMANDS::ERROR(command, message) => print_error(command, message),
+        };
+
 
     }
 }

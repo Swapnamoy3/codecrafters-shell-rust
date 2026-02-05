370522233fedca60d654b03c006b2c433ccda8a9
Swapnamoy Garai <garaiswap27@gmail.com>
2026-02-05 10:09:46 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
-
+    
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).unwrap();
+    let buffer = buffer.trim();
     println!("{}: command not found", buffer);
 }

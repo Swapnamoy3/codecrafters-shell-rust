b41d289287e35034e81f09b0b28e77c838fcf8c8
Swapnamoy Garai <garaiswap27@gmail.com>
2026-02-05 10:13:00 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::Command;
+
+
+fn input_command() -> String{
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let buffer = buffer.trim();
+
+    buffer.to_string()
+}
+
+
+fn print_error(command: String, message: String) {
+    println!("{}: {}", command, message);
+
+}
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
     
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).unwrap();
-    let buffer = buffer.trim();
-    println!("{}: command not found", buffer);
+    let command = input_command();
+    print_error(command, "command not found".to_string());
 }
